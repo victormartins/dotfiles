3def5b0684165b8f1c77de227c9b7b1935e432b8
Victor Martins <victor@ip-192-168-1-212.eu-west-1.compute.internal>
2021-01-13 18:21:41 +0000

    Added VLC to brew casks

--- v2/brew_casks.sh
+++ v2/brew_casks.sh
@@ -5,17 +5,18 @@ echo "Installing/Updating Casks..."
 brew install --cask brave-browser
 brew install --cask google-chrome
 brew install --cask firefox
-                  
+
 brew install --cask google-backup-and-sync
 brew install --cask dropbox
 
 brew install --cask keepassxc
 brew install --cask alfred
-                  
+
 brew install --cask skype
 brew install --cask zoom
 brew install --cask slack
-                  
+brew install --cask vlc
+
 brew install --cask caffeine
 brew install --cask amethyst
 brew install --cask alacritty
