42eedb966d498a28e753f89b201d04d5da4209cd
Victor Martins <correio@victormartins.com>
2021-02-05 22:55:23 +0000

    add extra fonts to pacman to have icons on linux

--- v2/brew_fonts.sh
+++ v2/brew_fonts.sh
@@ -4,3 +4,6 @@ brew install --cask font-fira-code
 brew install --cask font-source-code-pro
 brew install --cask font-hack-nerd-font
 brew install --cask font-fira-code-nerd-font
+sudo pacman -S ttf-nerd-fonts-symbols
+yay -S nerd-fonts-fira-code
+yay -S noto-fonts-emoji # allow icons on starship prompt
