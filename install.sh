261fc7dab3c52fed3bf854760a47584c83fc8d2f
Victor Martins <email@victormartins.com>
2022-04-20 18:29:29 +0100

    Add gitconfig to the symlinks

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 echo "Running Installation Script"
 
 # Symlink dotfiles
-ln -nsf $HOME/dotfiles/.profile  $HOME/.profile
-ln -nsf $HOME/dotfiles/.zprofile $HOME/.zprofile
-ln -nsf $HOME/dotfiles/.config $HOME/.config
+ln -nsf $HOME/dotfiles/.profile   $HOME/.profile
+ln -nsf $HOME/dotfiles/.zprofile  $HOME/.zprofile
+ln -nsf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -nsf $HOME/dotfiles/.config    $HOME/.config
   
 # Download Git Auto-Completion for Bash/ZSH
 curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > $HOME/.git-completion.bash
