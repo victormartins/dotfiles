71afdcf2dd4b4861c0b1c073b066f7dadbac862f
Victor Martins <email@victormartins.com>
2022-04-30 22:54:49 +0100

    Added TODO functions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,18 @@ require "user.options"
 require "user.keymaps"
 require "user.plugins"
 require "user.colorscheme"
+
+
+-- TODO:
+-- fun! TrimWhitespace()
+--     let l:save = winsaveview()
+--     keeppatterns %s/\s\+$//e
+--     call winrestview(l:save)
+-- endfun
+
+-- augroup clean_white_space
+--     autocmd!
+--     autocmd BufWritePre * :call TrimWhitespace()
+--     " autocmd VimEnter * :VimApm
+--     autocmd BufEnter,BufWinEnter,TabEnter *.rs :lua require'lsp_extensions'.inlay_hints{}
+-- augroup END
